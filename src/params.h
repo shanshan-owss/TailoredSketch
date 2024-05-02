40b4d04d60a75cef7ce8344320571428b4b7d258
QZRong <qzr18896809281@163.com>
2024-05-02 14:56:26 +0800

    upload TailoredSkecth Code

---
+++ src/params.h
@@ -0,0 +1,18 @@
+#ifndef TAILOREDSKETCH_CODE_PARAMS_H
+#define TAILOREDSKETCH_CODE_PARAMS_H
+
+#define COUNTER_SIZE 32    //counter_size
+
+#define WORD_SIZE 64
+
+#define MAX_HASH_NUM 16     //hash_number  = d
+
+#define FILTER_SIZE 32
+
+#define KEY_LEN 13
+
+#define testcycles 1
+
+
+
+#endif //TAILOREDSKETCH_CODE_PARAMS_H
